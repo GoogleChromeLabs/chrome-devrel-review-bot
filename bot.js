bc888dc23b3058f3d47e810a02ad2446e42ffe48
Sam Thorogood <sam.thorogood@gmail.com>
2021-08-19 16:18:25 +1000

    misc cleanup, remove 100 line length check

--- bot.js
+++ bot.js
@@ -1,16 +1,18 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // https://github.com/octokit/core.js#readme
 const { Octokit } = require('@octokit/core');
@@ -43,7 +45,12 @@ const actions = {
   }
 };
 
-const audit = async (org, repo, number) => {
+/**
+ * @param {string} org
+ * @param {string} repo
+ * @param {string|number} number
+ */
+async function audit(org, repo, number) {
   // Store data about the pull request.
   let data = {
     files: {},
@@ -74,7 +81,9 @@ const audit = async (org, repo, number) => {
       files.filter(file => file.filename.toLowerCase().endsWith('.png') ||
           file.filename.toLowerCase().endsWith('.jpg'));
   // Bail if the PR doesn't touch any content files.
-  if (newContent.length === 0 && modifiedContent.length === 0) return;
+  if (newContent.length === 0 && modifiedContent.length === 0) {
+    return;
+  }
 
   // Store data about the new or modified content.
   if (newContent.length > 0) {
