ac2d1adbaac448cb997315b2a4a685ad9100a417
Ewa Gasperowicz <devnook@gmail.com>
2022-04-11 13:40:01 +0200

    Add approval app logic

--- approvals/app.js
+++ approvals/app.js
@@ -1,31 +1,164 @@
+const micromatch = require('micromatch');
+const fetch = require('node-fetch');
+const YAML = require('yaml');
+
+const CHECK_RESULTS = {
+  success: {
+    conclusion: 'success',
+    title: 'Approvals complete',
+    summary: 'All required approvals are granted'
+  },
+  approval_missing: {
+    conclusion: 'failure',
+    title: 'Approvals missing',
+    summary: 'At least one approving review is required'
+  },
+  no_approvals: {
+
+  },
+  no_config: {
+    conclusion: 'cancelled',
+    title: 'Missing config file',
+    summary: '.github/chrome-devrel-bot.yml file does not exist'
+
+  },
+  no_files: {
+    conclusion: 'cancelled',
+    title: 'Missing PR files',
+    summary: 'No changed files found'
+  }
+}
+
 /**
+ * Main entrypoint to the Probot app
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  app.on("pull_request.edited", async (context) => {
-    const startTime = new Date();
-    let result = {
+
+  app.on([
+    'pull_request.opened',
+    'pull_request.reopened',
+    'pull_request.ready_for_review',
+    'pull_request.edited',
+  ], checkPR);
+
+  async function createCheck(octokit, checkOptions, result) {
+    return await octokit.checks.create({
+      ...checkOptions,
+      ...result,
+    });
+  }
+
+  function createCompletedResult({conclusion, title, summary}) {
+    return {
       status: 'completed',
-      conclusion: 'success',
+      conclusion,
       output: {
-        title: 'Approvals failed 444',
-        summary: `All required approvals are granted`
+        title,
+        summary
       }
     };
-    let owner = context.payload.repository.full_name.split('/')[0]
-    let repo = context.payload.repository.name;
-    let headSha = context.payload.pull_request.head.sha;
+  }
 
-    return await context.octokit.checks.create({
+  async function getFiles(octokit, checkOptions, pull_number) {
+    const filesRequest = await octokit.rest.pulls.listFiles({
+      owner: checkOptions.owner,
+      repo: checkOptions.repo,
+      pull_number,
+    });
+    if (!filesRequest.status === 200) {
+      result = createCompletedResult(CHECK_RESULTS.no_files);
+      await createCheck(octokit, checkOptions, result);
+      return false;
+    }
+    return filesRequest.data;
+  }
+
+  async function getReviews(octokit, checkOptions, pull_number) {
+    const reviewsRequest = await octokit.rest.pulls.listReviews({
+      owner: checkOptions.owner,
+      repo: checkOptions.repo,
+      pull_number,
+    });
+    if (!reviewsRequest.status === 200) {
+      result = createCompletedResult(CHECK_RESULTS.approval_missing);
+      await createCheck(octokit, checkOptions, result);
+      return false;
+    }
+    return reviewsRequest.data;
+  }
+
+  async function checkPR(context) {
+    const startTime = new Date();
+    const pull_request = context.payload.pull_request;
+    if (pull_request.state !== 'open' || pull_request.draft) {
+      return;
+    }
+
+    const octokit = context.octokit;
+    const headSha = pull_request.head.sha;
+    const owner = context.payload.repository.full_name.split('/')[0];
+    const repo = context.payload.repository.name;
+    const pull_number = pull_request.number;
+    let result;
+
+    const checkOptions = {
       headers: {
         accept: "application/vnd.github.v3+json"
       },
-      owner,
-      repo,
       name: "Chrome Devrel PR checks",
       started_at: startTime,
       head_sha: headSha,
-      ...result,
-    });
-  });
+      owner,
+      repo
+    };
+
+    // Get Config file
+    const url = `https://raw.githubusercontent.com/${owner}/${repo}/main/.github/chrome-devrel-bot.yml`;
+    const response = await fetch(url);
+    let config;
+    if (response.ok) {
+      const body = await response.text();
+      config = YAML.parse(body);
+    } else {
+      result = createCompletedResult(CHECK_RESULTS.no_config);
+      return await createCheck(octokit, checkOptions, result);
+    }
+
+    // Get PR files
+    const files = await getFiles(octokit, checkOptions, pull_number);
+    if (!files) {
+      return;
+    }
+    const paths = files.map(file => file.filename);
+
+    // Get PR reviews
+    const reviews = await getReviews(octokit, checkOptions, pull_number);
+    if (!reviews) {
+      return;
+    }
+    const approvers = reviews.map(review => {
+      return review.state === "APPROVED" ? review.user.login : false;
+    }).filter(approver => !!approver);
+    if (!approvers.length) {
+      result = createCompletedResult(CHECK_RESULTS.approval_missing);
+      return await createCheck(octokit, checkOptions, result);
+    }
+
+    result = createCompletedResult(CHECK_RESULTS.success);
+
+    for (const check of config.pr_checks.approvals) {
+      const isCheckRequired = micromatch(paths, check.paths, {'dot': true}).length > 0;
+      if (isCheckRequired) {
+        const checkApprovers = new Set(check.users);
+        const isApproved = approvers.filter(login => checkApprovers.has(login)).length > 0;
+        if (!isApproved) {
+          result = createCompletedResult(CHECK_RESULTS.approval_missing);
+          result.output.summary = `${check.check_name} failed`;
+          break;
+        }
+      }
+    }
+    return await createCheck(octokit, checkOptions, result);
+  }
 };
---
+++ test/approvals/app.test.js
@@ -0,0 +1,139 @@
+const fs = require('fs');
+const path = require('path');
+const nock = require('nock');
+// Requiring our app implementation
+const Approvals = require('../../approvals/app');
+const { Probot, ProbotOctokit } = require('probot');
+// Requiring our fixtures
+const payload = require('./fixtures/pull_request.edited');
+const checkRunSuccess = require('./fixtures/check_run.success');
+const checkRunNoApprovals = require('./fixtures/check_run.no_approvals');
+const checkRunApprovalMissing = require('./fixtures/check_run.approval_missing');
+
+
+const privateKey = fs.readFileSync(
+  path.join(__dirname, 'fixtures/mock-cert.pem'),
+  'utf-8'
+);
+
+describe('Approvals', () => {
+  let probot;
+  let mock;
+
+  const opts = {
+    'Content-Type': 'application/json',
+  };
+
+  beforeEach(() => {
+    nock.disableNetConnect();
+
+    // Mock Github requests
+    nock('https://raw.githubusercontent.com')
+      .get('/devnook/developer.chrome.com/main/.github/chrome-devrel-bot.yml')
+      .replyWithFile(200, __dirname + '/fixtures/config.yml')
+
+    mock = nock('https://api.github.com')
+      .post('/app/installations/24733797/access_tokens')
+      .reply(200, {
+        token: 'test',
+        permissions: {
+          checks: 'write',
+        },
+      })
+      // Mock files request
+      .get('/repos/devnook/developer.chrome.com/pulls/6/files', )
+      .replyWithFile(200, __dirname + '/fixtures/files.json', {
+        'Content-Type': 'application/json',
+      });
+
+    probot = new Probot({
+      appId: 123,
+      privateKey,
+      // disable request throttling and retries for testing
+      Octokit: ProbotOctokit.defaults({
+        retry: { enabled: false },
+        throttle: { enabled: false },
+      }),
+    });
+    // Load our app into probot
+    probot.load(Approvals);
+  });
+
+  test('Creates a SUCCESS check for approved PR', async () => {
+    mock
+      // Mock reviews request
+      .get('/repos/devnook/developer.chrome.com/pulls/6/reviews', )
+      .replyWithFile(200, __dirname + '/fixtures/reviews_approved.json', opts)
+      // Expected request
+      .post('/repos/devnook/developer.chrome.com/check-runs', (body) => {
+        body.started_at = '2018-10-05T17:35:21.594Z';
+        expect(body).toMatchObject(checkRunSuccess);
+        return true;
+      })
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: 'pull_request', payload });
+    expect(mock.pendingMocks()).toStrictEqual([]);
+  });
+
+  test('Creates a FAILURE check for a rejected PR', async () => {
+    mock
+      // Mock reviews request
+      .get('/repos/devnook/developer.chrome.com/pulls/6/reviews', )
+      .replyWithFile(200, __dirname + '/fixtures/reviews_rejected.json', opts)
+      // Expected request
+      .post('/repos/devnook/developer.chrome.com/check-runs', (body) => {
+        body.started_at = '2018-10-05T17:35:21.594Z';
+        expect(body).toMatchObject(checkRunNoApprovals);
+        return true;
+      })
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: 'pull_request', payload });
+    expect(mock.pendingMocks()).toStrictEqual([]);
+  });
+
+  test('Creates a FAILURE check for a missing content approval', async () => {
+    mock
+      // Mock reviews request
+      .get('/repos/devnook/developer.chrome.com/pulls/6/reviews', )
+      .replyWithFile(200, __dirname + '/fixtures/reviews_approved_rejected.json', opts)
+      // Expected request
+      .post('/repos/devnook/developer.chrome.com/check-runs', (body) => {
+        body.started_at = '2018-10-05T17:35:21.594Z';
+        expect(body).toMatchObject(checkRunApprovalMissing);
+        return true;
+      })
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: 'pull_request', payload });
+    expect(mock.pendingMocks()).toStrictEqual([]);
+  });
+
+
+  test('Creates a FAILURE check for a an unreviewed PR', async () => {
+    mock
+      // Mock reviews request
+      .get('/repos/devnook/developer.chrome.com/pulls/6/reviews', )
+      .replyWithFile(200, __dirname + '/fixtures/reviews_empty.json', opts)
+      // Expected request
+      .post('/repos/devnook/developer.chrome.com/check-runs', (body) => {
+        body.started_at = '2018-10-05T17:35:21.594Z';
+        expect(body).toMatchObject(checkRunNoApprovals);
+        return true;
+      })
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: 'pull_request', payload });
+    expect(mock.pendingMocks()).toStrictEqual([]);
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+});
